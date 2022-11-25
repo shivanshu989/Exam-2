ea0c3aa70c86a7a6b21981b2e04cd2eb0216d543
shivanshu989 <81761392+shivanshu989@users.noreply.github.com>
2022-11-25 23:16:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,51 +7,39 @@ document.getElementById('submit').onclick = loadDoc;
 
 
 function b1() {
-    // alert("clicked");
     document.getElementById('main').style.backgroundColor = 'blue';
     document.getElementById('text').style.color = 'skyblue';
-
-    
 }
 function b2() {
-    // alert("clicked");
     document.getElementById('main').style.backgroundColor = 'green';
     document.getElementById('text').style.color = 'greenyellow';
-
 }
 function b3() {
-    // alert("clicked");
     document.getElementById('main').style.backgroundColor = 'pink';
     document.getElementById('text').style.color = 'red';
-
 }
 function b4() {
-    // alert("clicked");
     document.getElementById('main').style.backgroundColor = 'skyblue';
     document.getElementById('text').style.color = 'blue';
-
 }
 function b5() {
-    // alert("clicked");
     document.getElementById('main').style.backgroundColor = 'grey';
     document.getElementById('text').style.color = 'black';
-
 }
-// ---------------------------------------------------------------------
+
 function loadDoc() {
     var name = document.getElementById('movie').value;
     document.getElementById('demo').innerHTML = '';
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        for(var i = 0; i < response.length; i++){
-          let r = response[i].show.image.medium;
-          document.getElementById('demo').innerHTML = document.getElementById('demo').innerHTML + `<img src ='${r}' />`;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            for (var i = 0; i < response.length; i++) {
+                let r = response[i].show.image.medium;
+                document.getElementById('demo').innerHTML = document.getElementById('demo').innerHTML + `<img src ='${r}' />`;
+            }
         }
-      }
     };
     xhttp.open("GET", `https://api.tvmaze.com/search/shows?q=${name}`, true);
     xhttp.send();
-  }
-// alert(b1);
+}
